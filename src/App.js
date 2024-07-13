a56be1cb59a9a4f945e20a9f805e541c4f6e246d
treva556 <marktreva92@gmail.com>
2024-07-13 18:54:04 +0300

    start

--- src/App.js
+++ src/App.js
@@ -8,10 +8,19 @@ function App() {
     <div className="App bg-slate-500">
       <Nav/>
    </div>
-   <div className=' p-32 text-white'>
-hh
-   </div>
+   <div className=' items-center p-24 text-black'>
+<p className=' text-5xl font-bold'> We Design</p>
+<p className='text-5xl font-bold' > We Design</p>
+<div>
+          <p className="inline  font-bold text-5xl">We </p>
+          <p className="inline text-red-500 font-bold text-5xl">Design</p>
+        </div>
+        </div>
+   <div className=' p-6 bg-slate-100'> 
+    h p
    <div className=' p-32 bg-black'>
+    pkk
+   </div>
    </div>
    </>
   );
