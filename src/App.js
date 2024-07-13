ccb12735110eb4e543fcd372d268d49000c7c6b0
treva556 <marktreva92@gmail.com>
2024-07-13 18:09:47 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Nav from './components/nav';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App bg-slate-500">
+      <Nav/>
+   </div>
+   <div className=' p-32 text-white'>
+hh
+   </div>
+   <div className=' p-32 bg-black'>
+   </div>
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+///
+
+import React from 'react';
+import '../App.css';
+
+function Nav() {
+  return (
+    <div className="App">
+    <p className=' fixed w-full  bg-red-700 h-10 text-white'> nav </p> 
+    </div>
+  );
+}
+
+export default Nav;
+
